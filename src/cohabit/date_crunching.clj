d451991938d6203dc1d4e37fe6f9d87d0778ebc0
bstee615 <benjaminjsteenhoek@gmail.com>
2024-01-29 12:44:27 -0600

    Add list of days to status output

--- src/cohabit/date_crunching.clj
+++ src/cohabit/date_crunching.clj
@@ -1,7 +1,8 @@
 ;;;; This namespace holds the functions for reading/writing to/from the database and calculating dates.
 
 (ns cohabit.date-crunching
-  (:require [cheshire.core :as json]
+  (:require [clojure.string :as string]
+            [cheshire.core :as json]
             [clj-time.core :as time]
             [clj-time.format :as fmt]
             [clj-time.coerce :as coerce])
@@ -39,10 +40,18 @@
     0
     data))
 
+(defn get-streak [data today]
+  (if (zero? (get-days-in-a-row data today))
+    (get-days-in-a-row data (time/minus today (time/days 1)))
+    (get-days-in-a-row data today)))
+
 (defn get-status [data today]
-  (let [days (if (zero? (get-days-in-a-row data today))
-              (get-days-in-a-row data (time/minus today (time/days 1)))
-              (get-days-in-a-row data today))
+  (let [days (get-streak data today)
         message (if (zero? days) "Shame" "Yay us")
         suffix (if (and (not-empty data) (not= today (coerce/to-local-date ((last (sort-by :date data)) :date)))) " Do it today!" "")]
-    (str "<div>" message "! We've kept our habit up for the last " days " days." suffix "</div>")))
+    (str "<div>"
+         "<div>" message "! We've kept our habit up for the last " days " days." suffix "</div>"
+         "<ul class=\"f-col dense\" role=\"list\">"
+         (string/join "" (map #(str "<li>" (% :date) "</li>") data))
+         "</ul>"
+         "</div>")))

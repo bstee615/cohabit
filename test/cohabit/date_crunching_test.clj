ff2b3f5dec68e751d1a135742763b0347aa047d7
bstee615 <benjaminjsteenhoek@gmail.com>
2024-01-29 12:32:48 -0600

    Make tests date-agnostic

--- test/cohabit/date_crunching_test.clj
+++ test/cohabit/date_crunching_test.clj
@@ -4,27 +4,29 @@
             [cohabit.date-crunching :refer :all]
             [clj-time.core :as time]))
 
+(def example-today (time/local-date 2024 01 05))
+
 (deftest date-math
   (testing "day math simple"
     (is (=
-         (time/day (time/minus (time/today) (time/days 1)))
-         28))))
+         (time/day (time/minus example-today (time/days 1)))
+         04))))
 
 (deftest streaks
   (testing "good streak"
-    (is (= (get-days-in-a-row (let [today (time/today)] [{:date today}
-                                                         {:date (time/minus today (time/days 1))}
-                                                         {:date (time/minus today (time/days 2))}]) (time/today))
+    (is (= (get-days-in-a-row (let [today example-today] [{:date today}
+                                                          {:date (time/minus today (time/days 1))}
+                                                          {:date (time/minus today (time/days 2))}]) example-today)
            3)))
   (testing "broken streak"
-    (is (= (get-days-in-a-row (let [today (time/today)] [{:date today}
-                                                         {:date (time/minus today (time/days 2))}
-                                                         {:date (time/minus today (time/days 3))}]) (time/today))
+    (is (= (get-days-in-a-row (let [today example-today] [{:date today}
+                                                          {:date (time/minus today (time/days 2))}
+                                                          {:date (time/minus today (time/days 3))}]) example-today)
            1)))
   (testing "not-started streak"
-    (is (= (get-days-in-a-row (let [today (time/today)] [{:date (time/minus today (time/days 1))}
-                                                         {:date (time/minus today (time/days 2))}
-                                                         {:date (time/minus today (time/days 3))}]) (time/today))
+    (is (= (get-days-in-a-row (let [today example-today] [{:date (time/minus today (time/days 1))}
+                                                          {:date (time/minus today (time/days 2))}
+                                                          {:date (time/minus today (time/days 3))}]) example-today)
            0))))
 
 (deftest database
